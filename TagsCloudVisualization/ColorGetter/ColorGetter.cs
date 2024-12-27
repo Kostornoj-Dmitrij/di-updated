a4c4cc39004ed23d81dacb27dc57eb4a2ac9386a
Kostornoj-Dmitrij <D1.imosa@yandex.ru>
2024-12-27 14:25:25 +0300

    Добавил класс с кастомными названиями цветов

--- TagsCloudVisualization/ColorGetter/ColorGetter.cs
+++ TagsCloudVisualization/ColorGetter/ColorGetter.cs
@@ -6,21 +6,17 @@ namespace TagsCloudVisualization.ColorGetter;
 public class ColorGetter : IColorGetter
 {
     private readonly ColorGetterProperties _colorGetterProperties;
-    private readonly Random _random;
 
     public ColorGetter(ColorGetterProperties colorGetterProperties)
     {
         _colorGetterProperties = colorGetterProperties;
-        _random = new Random();
     }
 
     public Color GetColor()
     {
-        if (_colorGetterProperties.ColorName == "random")
+        if (WellKnownColors.Colors.TryGetValue(_colorGetterProperties.ColorName, out var customColor))
         {
-            return Color.FromArgb(_random.Next(255), 
-                                _random.Next(255), 
-                                _random.Next(255));
+            return customColor;
         }
 
         return Color.FromName(_colorGetterProperties.ColorName);
